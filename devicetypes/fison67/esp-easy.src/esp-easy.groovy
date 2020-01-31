15770ed20bbbef82f77196a6cf37a7f0fb7f406a
chals <fison67@nate.com>
2020-01-31 10:51:58 +0900

    Update esp-easy.groovy

--- devicetypes/fison67/esp-easy.src/esp-easy.groovy
+++ devicetypes/fison67/esp-easy.src/esp-easy.groovy
@@ -26,7 +26,6 @@ metadata {
         capability "Relative Humidity Measurement"
         capability "Illuminance Measurement"
         capability "Carbon Dioxide Measurement"
-        capability "Dust Sensor"
         capability "Color Temperature"
 		capability "Dust Sensor" // fineDustLevel : PM 2.5   dustLevel : PM 10
         capability "Refresh"
